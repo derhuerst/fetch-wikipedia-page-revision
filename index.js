a18decb19b7836a63526154ce452f0ac16a99530
Jannis R <mail@jannisr.de>
2017-07-27 18:58:50 +0200

    copy from wikipedia-articles-feed

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
 'use strict'
 
-// todo
+const {stringify} = require('query-string')
+const {fetch} = require('fetch-ponyfill')({Promise: require('pinkie-promise')})
+const cheerio = require('cheerio')
+
+const endpoint = 'https://en.wikipedia.org/wiki/'
+
+const fetchPageRevision = (slug, revision) => {
+	const target = endpoint + slug + '?' + stringify({
+		useformat: 'mobile',
+		oldid: revision + ''
+	})
+
+	return fetch(target, {
+		mode: 'cors',
+		redirect: 'follow',
+		headers: {
+			'user-agent': 'https://gtihub.com/derhuerst/wikipedia-articles-feed'
+		}
+	})
+	.then((res) => {
+		if (!res.ok) {
+			const err = new Error(res.statusText)
+			err.statusCode = res.status
+			throw err
+		}
+		return res.text()
+	})
+	.then((html) => {
+		const $ = cheerio.load(html)
+
+		$('link[rel="stylesheet"]').remove()
+		$('link[rel="dns-prefetch"]').remove()
+		$('script').remove()
+		$('.banner-container').remove()
+		$('.header-container').remove()
+		$('.mw-revision').remove()
+
+		const fileLink = '/wiki/File:'
+		$('a.image').each((i, node) => {
+			if (node.attribs.href && node.attribs.href.slice(0, 11) === fileLink) {
+				const $a = $(node)
+				const isLazyLoaded = !!$a.find('.lazy-image-placeholder').length
+				if (isLazyLoaded) $a.html($a.find('noscript').text())
+			}
+		})
+
+		$('head').append($('<link rel="stylesheet" href="/static/mobile.css"/>'))
+
+		return $.html()
+	})
+}
+
+module.exports = fetchPageRevision
